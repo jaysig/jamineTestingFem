3ae840f2a39c4ec670e2c2ac2cd6e7dbb4276c64
Jaysig <jasontsigmon@gmail.com>
2017-01-24 11:52:06 -0600

    Added test for III that shows repeatability

--- standalone/src/roman-numeral-converter.js
+++ standalone/src/roman-numeral-converter.js
@@ -1,5 +1,11 @@
 window.ConvertsNumerals = function() {
-  this.fromRoman = function() {
-    
+  this.fromRoman = function(input) {
+    if(input === "I") {
+      return 1;
+    } else if(input === "II") {
+      return 2;
+    } else {
+      return 3;
+    }
   }
 };
